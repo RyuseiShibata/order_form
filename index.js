58ce9d93ed0a63be5fb38b1d1735ffdda7e8b1ca
RyuseiShibata <67996465+RyuseiShibata@users.noreply.github.com>
2022-07-08 23:06:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function subForm() {
         console.log(i);
     
         for(let k=0; k<i; k++){
-            msg = `【注文内容】\n日時：${Year}年${Month}月${Date1}日${Hour}時${Min}分\n 商品名：${item_name[k]}\n 個数：${num[k]}\n 納期：${date[k]}\n 備考：${note[k]}`;
+            msg = `【注文内容】\n 日時：${Year}年${Month}月${Date1}日${Hour}時${Min}分\n 商品名：${item_name[k]}\n 個数：${num[k]}\n 納期：${date[k]}\n 備考：${note[k]}`;
             //console.log(msg);
             setTimeout(sendText(msg), 1000);
         }
